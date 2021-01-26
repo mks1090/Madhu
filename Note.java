7fae669b402dad8d3c6635aacb68704f6cac5cf1
mks1090 <75899435+mks1090@users.noreply.github.com>
2021-01-26 15:06:29 +0530

    Add files via upload

---
+++ Note.java
@@ -0,0 +1,54 @@
+package com.entities;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+	@Id
+	private int id;
+	private String tite;
+	private String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTite() {
+		return tite;
+	}
+	public void setTite(String tite) {
+		this.tite = tite;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getAddedDate() {
+		return addedDate;
+	}
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+	private Date addedDate;
+	public Note(int id, String tite, String content, Date addedDate) {
+		super();
+		this.id = new Random().nextInt(100000);
+		this.tite = tite;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
